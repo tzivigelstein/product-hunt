120d1c3a45b58dfe91afcb2b0fa84347f9977ec6
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-09-27 21:00:17 -0300

    refactor header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,28 +1,25 @@
-import {useContext} from "react"
+import { useContext } from "react"
 import Search from "../UI/Search"
 import Nav from "./Nav"
 import Link from "next/link"
 import Button from "../UI/Button"
 import styled from "@emotion/styled"
-import {css} from "@emotion/core"
+import { css } from "@emotion/core"
 import FirebaseContext from "../../firebase/context"
 
-const HeaderContainer = styled.div`
-  max-width: 1200px;
-  width: 95%;
-  margin: 0 auto;
+const HeaderContainer = styled.header`
+  border-bottom: 1px solid var(--light-gray);
+  padding: 1rem 2rem;
+  background-color: #fff;
+
   @media (min-width: 768px) {
     display: flex;
     justify-content: space-between;
   }
 `
 
-const Logo = styled.a`
-  color: var(--naranja);
-  font-size: 4rem;
-  line-height: 0;
-  font-weight: 700;
-  margin-right: 2rem;
+const ProductHuntLogo = styled.img`
+  height: 40px;
 `
 
 const NavContainer = styled.div`
@@ -36,64 +33,62 @@ const NavContainer = styled.div`
 `
 
 const Header = () => {
-  const {user, firebase} = useContext(FirebaseContext)
+  const { user, firebase } = useContext(FirebaseContext)
 
   return (
-    <header
-      css={css`
-        border-bottom: 2px solid var(--gris3);
-        padding: 1rem 0;
-        background-color: #fff;
-      `}
-    >
-      <HeaderContainer>
-        <NavContainer>
-          <div
-            css={css`
-              display: flex;
-              align-items: center;
-            `}
-          >
-            <Link href="/">
-              <Logo>P</Logo>
-            </Link>
-            <Search />
-          </div>
-          <Nav />
-        </NavContainer>
+    <HeaderContainer>
+      <NavContainer>
         <div
           css={css`
             display: flex;
             align-items: center;
-            justify-content: space-around;
+            gap: 2rem;
           `}
         >
-          {user ? (
-            <>
-              <p
-                css={css`
-                  margin-right: 2rem;
-                `}
-              >
-                Bienvenido, {user.displayName}
-              </p>
-              <Button bgColor="true" onClick={() => firebase.signout()}>
-                Cerrar sesión
-              </Button>
-            </>
-          ) : (
-            <>
-              <Link href="/login">
-                <Button bgColor="true">Iniciar sesión</Button>
-              </Link>
-              <Link href="/signup">
-                <Button style={{color: "#222", borderColor: "#222"}}>Registrarse</Button>
-              </Link>
-            </>
-          )}
+          <Link href="/">
+            <ProductHuntLogo
+              src="/product-hunt-full.svg"
+              alt="Product Hunt Logo"
+            />
+          </Link>
+          <Search />
         </div>
-      </HeaderContainer>
-    </header>
+        <Nav />
+      </NavContainer>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-around;
+        `}
+      >
+        {user ? (
+          <>
+            <p
+              css={css`
+                margin-right: 2rem;
+              `}
+            >
+              Bienvenido, {user.displayName}
+            </p>
+            <Button bgColor="true" onClick={() => firebase.signout()}>
+              Cerrar sesión
+            </Button>
+          </>
+        ) : (
+          <>
+            <Link href="/login">
+              <Button bgColor="true">Iniciar sesión</Button>
+            </Link>
+            <Link href="/signup">
+              <Button style={{ color: "#222", borderColor: "#222" }}>
+                Registrarse
+              </Button>
+            </Link>
+          </>
+        )}
+      </div>
+    </HeaderContainer>
   )
 }
 
