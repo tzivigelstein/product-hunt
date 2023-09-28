54f6a9949b5d96914fad2d507a311b48c2d0086c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-09-27 21:00:27 -0300

    remove unused styles

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -6,62 +6,6 @@ import Header from "./Header"
 const Layout = props => {
   return (
     <>
-      <Global
-        styles={css`
-          :root {
-            --gris: #3d3d3d;
-            --gris2: #6f6f6f;
-            --gris3: #e1e1e1;
-            --naranja: #da552f;
-          }
-
-          *,
-          *:before,
-          *after {
-            box-sizing: border-box;
-          }
-
-          html {
-            font-size: 62.5%;
-            box-sizing: inherit;
-          }
-
-          body {
-            font-family: "PT Sans", sans-serif;
-            font-size: 1.6rem;
-            line-height: 1.5;
-          }
-
-          h1,
-          h2,
-          h3 {
-            margin: 0 0 2rem 0;
-            line-height: 1.5;
-          }
-          h1,
-          h2 {
-            font-weight: 700;
-          }
-
-          h3 {
-            font-family: "PT Sans", sans-serif;
-          }
-
-          ul {
-            list-style: none;
-            margin: 0;
-            padding: 0;
-          }
-
-          a {
-            text-decoration: none;
-          }
-
-          img {
-            max-width: 100%;
-          }
-        `}
-      />
       <Head>
         <title>Product Hunt - The best new products in tech.</title>
         <link
