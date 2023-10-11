230593db4066a2e2fbd0d86a40bcf0bcf06e4ccc
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-10-11 17:48:44 -0300

    change background-color for a custom property and update font-weight

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -4,13 +4,12 @@ const Button = styled.a`
   margin: 0 0.5rem;
   font-size: 13px;
   display: block;
-  font-weight: 700;
   text-transform: uppercase;
   border: 1px solid #d1d1d1;
   border-radius: 6px;
   padding: 0.6rem 1.5rem;
   text-align: center;
-  background-color: ${props => (props.bgColor ? '#DA552F' : 'white')};
+  background-color: ${props => (props.bgColor ? 'var(--orange)' : 'white')};
   color: ${props => (props.bgColor ? 'white' : '#000')};
 
   &:hover {
