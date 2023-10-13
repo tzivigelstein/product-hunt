930e52bd7e547dd3d5b3cff72e1eea708e5814e6
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-10-13 12:51:51 -0300

    create profile api controller

---
+++ pages/api/profile/[username].js
@@ -0,0 +1,30 @@
+import admin from "firebase-admin"
+
+export default async function handler(req, res) {
+  const { username } = req.query
+
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+      }),
+    })
+  }
+
+  try {
+    const { users } = await admin.auth().listUsers()
+
+    const userRecord = users.find(
+      user => user.displayName.toLowerCase() === username
+    )
+
+    console.log(users)
+
+    res.status(200).json({ userRecord })
+  } catch (error) {
+    console.error("Error fetching user data:", error)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+}
